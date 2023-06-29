ac1f4b3552829e94002a00419b594778250f2292
Cleo <cleo.debeau@gmail.com>
2023-06-29 05:06:50 -0400

    Added PWA config

--- routes/index.jsx
+++ routes/index.jsx
@@ -7,12 +7,24 @@ export default function Home() {
       <Head>
         <title>Mebious</title>
         <link rel="stylesheet" href="/main.css"></link>
+        <link
+          crossorigin="use-credentials"
+          rel="manifest"
+          href="/manifest.webmanifest"
+        />
       </Head>
       <Sidebar />
       <div class="p-4 mx-auto max-w-screen-md">
         <h1 class="text-3xl font-bold">Mebious</h1>
         <a href="/search/">Search</a>
         <a href="/random/">random</a>
+
+        <script type="module">
+          import
+          "https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate/dist/pwa-update.js";
+          const el = document.createElement("pwa-update");
+          document.body.appendChild(el);
+        </script>
       </div>
     </>
   );
